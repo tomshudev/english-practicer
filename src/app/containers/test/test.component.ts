ce3f03f714ee002cc03a98fb1af61345b5b07405
tomshudev <tomshudev@gmail.com>
2020-02-10 23:47:26 +0200

    Created the test comp

--- src/app/containers/test/test.component.ts
+++ src/app/containers/test/test.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from "@angular/core";
-import { TestService } from "src/app/services/test.service";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { TestService, Question } from "src/app/services/test.service";
+import { FormGroup } from "@angular/forms";
+import { MatStepper, MatSnackBar } from "@angular/material";
 
 @Component({
   selector: "app-test",
@@ -7,7 +9,20 @@ import { TestService } from "src/app/services/test.service";
   styleUrls: ["./test.component.less"]
 })
 export class TestComponent implements OnInit {
+  questions: Question[] = this.testService.currentTest;
+  questionFormGroup: FormGroup;
+  currStep: number = 0;
+
+  score: number = 0;
+
+  @ViewChild(MatStepper, { static: false }) stepper: MatStepper;
+
   constructor(private testService: TestService) {}
 
   ngOnInit() {}
+
+  finishedQuestion(isCorrectAnswer: boolean) {
+    this.score += isCorrectAnswer ? 1 : 0;
+    this.stepper.next();
+  }
 }
